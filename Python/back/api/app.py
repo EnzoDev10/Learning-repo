329eb0d83c746a1c9a16bc4d88e9e26c6dfead45
Enzo Cañete <enzodev4@gmail.com>
2024-01-02 09:59:35 -0300

    Flask tutorial
    on how to GET data from an API.

--- Python/back/api/app.py
+++ Python/back/api/app.py
@@ -1,15 +1,14 @@
 from flask import Flask, render_template
 import requests
 import json
-
-
     
 app = Flask(__name__, template_folder=r"C:\Users\Administrator\Desktop\coding\repos\Learning-repo\Python\back\api\templates")
-
-
+ 
+""" Get is the default value of methods but as a good redability practice it should be specified."""
 @app.route("/", methods=["GET"])
 def index():
     req = requests.get("https://cat-fact.herokuapp.com/facts")
-    data = req.content
-    return render_template(r"index.html", data=data)
+    #By using json the data can be accessed as a dictionary.
+    data = json.loads(req.content)
+    return render_template(r"index.html", data=data)   
 
