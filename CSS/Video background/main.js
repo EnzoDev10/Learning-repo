84a76cebc9c8e89439df74770bdfa87a7844bb2b
Enzo Cañete <enzodev4@gmail.com>
2024-01-31 16:26:46 -0300

    Tutorial Version
    The challenge begins

--- CSS/Video background/main.js
+++
@@ -1,7 +0,0 @@
-const showcase = document.querySelector(".showcase");
-const menuToggle = document.querySelector(".toggle");
-
-menuToggle.addEventListener("click", () => {
-    menuToggle.classList.toggle("active")
-    showcase.classList.toggle("active")
-})
