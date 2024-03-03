98a13cd65ed4b43e5707f598437bfdef7d50e597
Enzo Cañete <enzodev4@gmail.com>
2024-03-03 15:23:29 -0300

    Added some stuff

--- Python/API/weather/main.py
+++ Python/API/weather/main.py
@@ -1,34 +1,36 @@
 import datetime as dt
 import requests
+import json
 
 base_url = "http://api.openweathermap.org/data/2.5/weather?appid=23285a4497af200a1a66a8d602346440"
 
-city = "José C. Paz"
+city = "Buenos Aires"
 
 
-def kelvin_to_celsius_fahrenheit(kelvin):
+def kelvin_to_celsius(kelvin):
     celsius = kelvin - 273.15
-    fahrenheit = celsius * (9 / 5) + 32
-    return celsius, fahrenheit
+    return celsius
 
 
 url = base_url + "&q=" + city
 
 response = requests.get(url).json()
 
-#print(response)
+print(json.dumps(response, indent=4))
 
-# 1st access a category, 2nd the index inside and 3rd, access the specific key
+desc = response["weather"][0]["description"]
 
-desc = response['weather'][0]['description']
-temp_min_kelvin = response['main']['temp_min']
-temp_max_kelvin = response['main']['temp_max']
+current_temp_kelvin = response["main"]["temp"]
 
-temp_min_celsius, temp_min_fahrenheit = kelvin_to_celsius_fahrenheit(temp_min_kelvin)
-temp_max_celsius, temp_max_fahrenheit = kelvin_to_celsius_fahrenheit(temp_max_kelvin)
+current_temp_celsius = kelvin_to_celsius(current_temp_kelvin)
 
-print(response,"\n")
+temp_min_kelvin = response["main"]["temp_min"]
+temp_max_kelvin = response["main"]["temp_max"]
+temp_min_celsius = kelvin_to_celsius(temp_min_kelvin)
+temp_max_celsius = kelvin_to_celsius(temp_max_kelvin)
+
 
 print(f"Right now we have a {desc}.")
-print(f"The minimum temperature for today is {temp_min_celsius:.1f} °C")
+print(f"The minimum temperature for today is {temp_min_celsius:.1f} °C (Probably wrong)")
 print(f"The maximum temperature for today is {temp_max_celsius:.1f} °C")
+print(f"The temperature right now is {current_temp_celsius:.1F} °C")
