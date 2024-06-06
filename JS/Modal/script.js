b311288fe971862a53c4aa84648ac453fc7f65c5
Enzo Cañete <enzodev4@gmail.com>
2024-06-06 16:04:40 -0300

    Added a modal example

---
+++ JS/Modal/script.js
@@ -0,0 +1,29 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openBtn = document.querySelector(".btn-open");
+const closeBtn = document.querySelector(".btn-close");
+
+function toggleModal() {
+	if (modal.classList.contains("hidden")) {
+		modal.classList.remove("hidden");
+		overlay.classList.remove("hidden");
+	}
+}
+
+function close() {
+	modal.classList.add("hidden");
+	overlay.classList.add("hidden");
+}
+
+openBtn.addEventListener("click", toggleModal);
+
+closeBtn.addEventListener("click", close);
+
+overlay.addEventListener("click", close);
+
+/* If the user presses "Esc" and the modal is open, it closes it */
+document.addEventListener("keydown", function (e) {
+	if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+		close();
+	}
+});
