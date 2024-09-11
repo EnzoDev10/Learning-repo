0b9286948977669a16be533306ab2e4f2ff84c7d
Enzo Cañete <enzodev4@gmail.com>
2024-09-11 17:51:08 -0300

    changed a folders name

--- JS/Modal/script.js
+++ JS/Modal/script.js
@@ -1,29 +1,30 @@
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const openBtn = document.querySelector(".btn-open");
-const closeBtn = document.querySelector(".btn-close");
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const openBtn = document.querySelector('.btn-open');
+const closeBtn = document.querySelector('.btn-close');
 
 function toggleModal() {
-	if (modal.classList.contains("hidden")) {
-		modal.classList.remove("hidden");
-		overlay.classList.remove("hidden");
+	// if the modal is not visible, opens it and shows the blur effect
+	if (modal.classList.contains('hidden')) {
+		modal.classList.remove('hidden');
+		overlay.classList.remove('hidden');
 	}
 }
 
 function close() {
-	modal.classList.add("hidden");
-	overlay.classList.add("hidden");
+	modal.classList.add('hidden');
+	overlay.classList.add('hidden');
 }
 
-openBtn.addEventListener("click", toggleModal);
+openBtn.addEventListener('click', toggleModal);
 
-closeBtn.addEventListener("click", close);
+closeBtn.addEventListener('click', close);
 
-overlay.addEventListener("click", close);
+overlay.addEventListener('click', close);
 
 /* If the user presses "Esc" and the modal is open, it closes it */
-document.addEventListener("keydown", function (e) {
-	if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+document.addEventListener('keydown', function (e) {
+	if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
 		close();
 	}
 });
